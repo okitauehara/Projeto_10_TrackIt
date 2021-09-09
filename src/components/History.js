4c92cd150c203137ad4f79a55d0262dcbac0389f
okitauehara <marcosokita@live.com>
2021-09-09 16:24:19 -0300

    Histórico: layout base concluído

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,21 @@
+import styled from "styled-components";
+
 export default function History() {
     return (
         <main>
-            <p>History</p>
+            <HabitHistory>Histórico</HabitHistory>
+            <NoHistoryAlert>Em breve você poderá ver o histórico dos seus hábitos aqui!</NoHistoryAlert>
         </main>
     );
 }
+
+const HabitHistory = styled.h1`
+    font-size: 23px;
+    color: #126ba5;
+    margin-bottom: 30px;
+`;
+
+const NoHistoryAlert = styled.p`
+    font-size: 18px;
+    color: #666666;
+`;
